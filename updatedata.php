25819123416f805f6d69743f5a391bb6a496d24b
unknown <arifdewangga12@gmail.com>
2020-09-29 23:45:30 -0700

    edit

--- updatedata.php
+++ updatedata.php
@@ -7,8 +7,7 @@ include "connect.php";
    $merksepeda = $_POST['merksepeda'];
    $jenissepeda = $_POST['jenissepeda'];
    $warnasepeda = $_POST['warnasepeda'];
-   $hargasewa  = $_POST['hargasewa'];
-   $gambarsepeda = $_POST['gambarsepeda'];
+   $hargasewa  = $_POST['hargasewa']
 
    if (isset($id) &&isset($namasepeda) && isset($kodesepeda) && isset($merksepeda) && isset($jenissepeda) && isset($warnasepeda) && isset($hargasewa) && isset($gambarsepeda)) {
      $query="UPDATE user SET nim='$nim',nama='$nama',jurusan='$jurusan',jenis_kelamin='$jenis_kelamin',alamat='$alamat' where id_mahasiswa='$id_mahasiswa'";
