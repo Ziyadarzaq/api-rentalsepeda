1c127d2b8be654094b156ada08f1af67de4c3fcd
arifdewangga12 <arifdewangga2@gmail.com>
2020-12-03 02:22:15 -0800

    data

--- updatedata.php
+++ updatedata.php
@@ -8,11 +8,18 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
     $jenissepeda = $_POST['jenissepeda'];
     $warnasepeda = $_POST['warnasepeda'];
 		$hargasewa = $_POST['hargasewa'];
+    $filename = $_FILES['gambarsepeda']['name'];
 
+    $filetmpname = $_FILES['gambarsepeda']['tmp_name'];
+
+			// FOLDER DIMANA GAMBAR AKAN DI SIMPAN
+			$folder = 'img/';
+			// GAMBAR DI SIMPAN KE DALAM FOLDER
+			move_uploaded_file($filetmpname, $folder . $filename);
 
     require_once 'connect.php';
 
-    $sql = "UPDATE sepeda SET namasepeda='$namasepeda',kodesepeda='$kodesepeda',jenissepeda='$jenissepeda',merksepeda='$merksepeda', warnasepeda='$warnasepeda',hargasewa='$hargasewa' WHERE id = '$id'";
+    $sql = "UPDATE sepeda SET namasepeda='$namasepeda',kodesepeda='$kodesepeda',jenissepeda='$jenissepeda',merksepeda='$merksepeda', warnasepeda='$warnasepeda',hargasewa='$hargasewa' ,gambarsepeda ='$filename' WHERE id = '$id'";
 
 
     if( mysqli_query($conn, $sql) ) {
